853485f714ec64d2df6a39bbd59d83ad1e14c5f5
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-14 18:23:03 +0530

    XAMARIN-34533 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,232 @@
+﻿using Syncfusion.SfNavigationDrawer.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+        int flag = 0;
+        public Position Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged("Position");
+            }
+        }
+        public bool IsOpen
+        {
+            get => _isOpen;
+            set
+            {
+                _isOpen = value;
+                OnPropertyChanged("IsOpen");
+            }
+        }
+        public Command ShowListView { get; set; }
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            ShowListView = new Command(OnShowListView);
+            contactsinfo = new ObservableCollection<Contacts>();
+            GenerateInfo();
+        }
+
+        private void OnShowListView(object obj)
+        {
+            if (flag > 3)
+                flag %= 4;
+            switch (flag)
+            {
+                case 0:
+                    Position = Position.Left;
+                    break;
+                case 1:
+                    Position = Position.Bottom;
+                    break;
+                case 2:
+                    Position = Position.Right;
+                    break;
+                case 3:
+                    Position = Position.Top;
+                    break;
+                default:
+                    break;
+            }
+            IsOpen = true;
+            flag++;
+        }
+
+        public void GenerateInfo()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                var contact = new Contacts(CustomerNames[i], r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString());
+                contact.ContactImage = ImageSource.FromResource("ListViewXamarin.Images.Image" + r.Next(0, 28) + ".png");
+                contactsinfo.Add(contact);
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Jack",
+            "Liz",
+            "Zeke",
+            "Noah",
+            "Pete",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason",
+            "Liam",
+            "Jacob",
+            "Jayden",
+            "Ethan",
+            "Noah",
+            "Lucas",
+            "Logan",
+            "Caleb",
+            "Caden",
+            "Jack",
+            "Ryan",
+            "Connor",
+            "Michael",
+            "Elijah",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James",
+            "Landon",
+            "Nathan",
+            "Dylan",
+            "Evan",
+            "Luke",
+            "Andrew",
+            "Gabriel",
+            "Gavin",
+            "Joshua",
+            "Owen",
+            "Daniel",
+            "Carter",
+            "Tyler",
+            "Cameron",
+            "Christian",
+            "Wyatt",
+            "Henry",
+            "Eli",
+            "Joseph",
+            "Max",
+            "Isaac",
+            "Samuel",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David",
+            "Christopher",
+            "John",
+            "Isaiah",
+            "Levi",
+            "Jonathan",
+            "Oliver",
+            "Chase",
+            "Cooper" ,
+            "Tristan",
+            "Colton",
+            "Austin",
+            "Colin",
+            "Charlie",
+            "Dominic",
+            "Parker",
+            "Hunter",
+            "Thomas",
+            "Alex",
+            "Ian",
+            "Jordan",
+            "Cole",
+            "Julian",
+            "Aaron",
+            "Carson",
+            "Miles",
+            "Blake",
+            "Brody",
+            "Adam",
+            "Sebastian",
+            "Adrian",
+            "Nolan",
+            "Sean",
+            "Riley",
+            "Bentley",
+            "Xavier",
+            "Hayden",
+            "Jeremiah",
+            "Jason",
+            "Jake",
+            "Asher",
+            "Micah",
+            "Jace",
+            "Brandon",
+            "Josiah",
+            "Hudson",
+            "Nathaniel",
+            "Bryson",
+            "Ryder",
+            "Justin",
+            "Bryce",
+        };
+        private bool _isOpen;
+        private Position _position;
+
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        #endregion
+    }
+}
